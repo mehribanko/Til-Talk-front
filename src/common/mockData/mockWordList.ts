6a80adecd0541184bc4431bf2aaf9d3905ff32ef
Mehriban/Mareeko <mehrikodes@gmail.com>
2026-02-20 17:42:51 +0900

    changing mock data, adding lang type

--- src/common/mockData/mockWordList.ts
+++ src/common/mockData/mockWordList.ts
@@ -1,38 +1,162 @@
 export const mockData = [
-    {
-        "lang": "Korean",
-        "korean": "안녕하세요",
-        "romanization": "annyeonghaseyo",
-        "karakalpak": "Sa'lem!",
+  {
+    "id": 1,
+    "level": "Beginner",
+    "category": "Greetings",
+    "korean": {
+      "word": "안녕하세요",
+      "romanization": "Annyeong-haseyo",
+      "audio_url": "/audio/kr/hello.mp3"
     },
-    {
-        "lang": "Korean",
-        "korean": "감사합니다",
-        "romanization": "gamsahamnida",
-        "karakalpak": "Raxmet",
+    "karakalpak": {
+      "word": "Assalawma áleykum",
+      "romanization": "Assalawma áleykum",
+      "audio_url": "/audio/kk/hello.mp3"
     },
-    {
-        "lang": "Korean",
-        "korean": "네",
-        "romanization": "ne",
-        "karakalpak": "Awa",
+    "part_of_speech": "Greeting"
+  },
+  {
+    "id": 2,
+    "level": "Beginner",
+    "category": "Food",
+    "korean": {
+      "word": "물",
+      "romanization": "Mul",
+      "audio_url": "/audio/kr/water.mp3"
     },
-    {
-        "lang": "Korean",
-        "korean": "아니요",
-        "romanization": "aniyo",
-        "karakalpak": "Joq",
+    "karakalpak": {
+      "word": "Suw",
+      "romanization": "Suw",
+      "audio_url": "/audio/kk/water.mp3"
     },
-    {
-        "lang": "Korean",
-        "korean": "주세요",
-        "romanization": "juseyo",
-        "karakalpak": "Berip jiberin'!",
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 3,
+    "level": "Beginner",
+    "category": "Food",
+    "korean": {
+      "word": "빵",
+      "romanization": "Pbang",
+      "audio_url": "/audio/kr/bread.mp3"
     },
-    {
-        "lang": "Korean",
-        "korean": "도와주세요",
-        "romanization": "dowajuseyo",
-        "karakalpak": "Ja'rdem berin'.",
-    }
+    "karakalpak": {
+      "word": "Nan",
+      "romanization": "Nan",
+      "audio_url": "/audio/kk/bread.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 4,
+    "level": "Beginner",
+    "category": "People",
+    "korean": {
+      "word": "친구",
+      "romanization": "Chingu",
+      "audio_url": "/audio/kr/friend.mp3"
+    },
+    "karakalpak": {
+      "word": "Dos",
+      "romanization": "Dos",
+      "audio_url": "/audio/kk/friend.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 5,
+    "level": "Beginner",
+    "category": "Education",
+    "korean": {
+      "word": "학교",
+      "romanization": "Hakgyo",
+      "audio_url": "/audio/kr/school.mp3"
+    },
+    "karakalpak": {
+      "word": "Mektep",
+      "romanization": "Mektep",
+      "audio_url": "/audio/kk/school.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 6,
+    "level": "Beginner",
+    "category": "Education",
+    "korean": {
+      "word": "책",
+      "romanization": "Chaek",
+      "audio_url": "/audio/kr/book.mp3"
+    },
+    "karakalpak": {
+      "word": "Kitap",
+      "romanization": "Kitap",
+      "audio_url": "/audio/kk/book.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 7,
+    "level": "Beginner",
+    "category": "Education",
+    "korean": {
+      "word": "선생님",
+      "romanization": "Seonsaeng-nim",
+      "audio_url": "/audio/kr/teacher.mp3"
+    },
+    "karakalpak": {
+      "word": "Muģallim",
+      "romanization": "Muģallim",
+      "audio_url": "/audio/kk/teacher.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 8,
+    "level": "Beginner",
+    "category": "Education",
+    "korean": {
+      "word": "학생",
+      "romanization": "Hak-saeng",
+      "audio_url": "/audio/kr/student.mp3"
+    },
+    "karakalpak": {
+      "word": "Oqıwshı",
+      "romanization": "Oqıwshı",
+      "audio_url": "/audio/kk/student.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 9,
+    "level": "Beginner",
+    "category": "Home",
+    "korean": {
+      "word": "집",
+      "romanization": "Jip",
+      "audio_url": "/audio/kr/house.mp3"
+    },
+    "karakalpak": {
+      "word": "Úy",
+      "romanization": "Úy",
+      "audio_url": "/audio/kk/house.mp3"
+    },
+    "part_of_speech": "Noun"
+  },
+  {
+    "id": 10,
+    "level": "Beginner",
+    "category": "Home",
+    "korean": {
+      "word": "가족",
+      "romanization": "Gajok",
+      "audio_url": "/audio/kr/family.mp3"
+    },
+    "karakalpak": {
+      "word": "Shańaraq",
+      "romanization": "Shańaraq",
+      "audio_url": "/audio/kk/family.mp3"
+    },
+    "part_of_speech": "Noun"
+  }
 ]
--- src/pages/LearnWordMenu.tsx
+++ src/pages/LearnWordMenu.tsx
@@ -2,11 +2,14 @@ import LearnWordCard from "../components/card/LearnWordCard"
 import {useState} from "react";
 import {mockData} from "../common/mockData/mockWordList.ts";
 import {StatCard} from "../components/card/StatCard.tsx";
+import {LANG_CONFIG} from "../common/constant/MenuData.ts";
+
 
 export const LearnWordMenu = () => {
 
     const [currentIdx, setCurrentIdx] = useState(0);
     const isComplete = currentIdx === mockData.length;
+    const [learnLangType]= useState<'kor' | 'kk'>('kk');
 
     const currentWord = mockData[currentIdx];
     const handleOnLearnClick = () => {
@@ -18,16 +21,21 @@ export const LearnWordMenu = () => {
             <div> You learned all the words! </div>
         )
     }
+
+    const config = LANG_CONFIG[learnLangType];
+    const langData = currentWord[config.key];
+
+
     return (
             <div className="h-full flex flex-col">
                 <StatCard />
 
                 <div className="flex-1 flex items-center justify-center">
-                   <LearnWordCard
-                        lang={currentWord.lang}
-                        text={currentWord.korean}
-                        pronunciation={currentWord.romanization}
-                        onLearnClick = {handleOnLearnClick} />
+                    <LearnWordCard
+                        lang={config.label}
+                        text={langData.word}
+                        pronunciation={langData.romanization}
+                        onLearnClick={handleOnLearnClick} />
                 </div>
         </div>
     )
